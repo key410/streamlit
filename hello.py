37128dfe796116bc98290db4b44a6edf242968e8
key410 <yuumiya5@gmail.com>
2023-07-11 13:30:05 +0900

    #1 ハローワールド

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
+# 1.Hello world
 import streamlit as st
 
+# タイトル、ヘッダー、テキストを出力
 st.title("Hello world!!")
 st.header("header")
 st.write("text write")
 
+# streamlitの各部品を出力
 st.button("ボタン")
 st.selectbox("select", ("sel1", "sel2"))
 st.multiselect("multiselect", ("sel1", "sel2"))
